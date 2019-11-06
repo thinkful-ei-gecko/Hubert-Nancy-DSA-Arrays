9be6f1a9775035bc5ac15f438d772ba7e32e7eaf
Hubert Yang <offspring7890@yahoo.com>
2019-11-06 09:48:42 -0800

    created myArray class

---
+++ myArray.js
@@ -0,0 +1,30 @@
+const Memory = require('./Memory')
+let memory = new Memory();
+
+class myArray {
+  constructor() {
+    this.length = 0;
+    this._capacity = 0;
+    this.ptr = memory.allocate(this.length);
+  }
+
+  _resize(size) {
+    const oldPtr = this.ptr;
+    this.ptr = memory.allocate(size);
+    if (this.ptr === null) {
+        throw new Error('Out of memory');
+    }
+    memory.copy(this.ptr, oldPtr, this.length);
+    memory.free(oldPtr);
+  }
+
+  push(value) {
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * myArray.SIZE_RATIO)
+    }
+    memory.set(this.ptr + this.length, value);
+    this.length++
+  }
+}
+
+module.exports = myArray;
